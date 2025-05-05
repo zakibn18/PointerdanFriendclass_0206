dd11c62cec80fe1f7b6e3e2d03748427e6213653
zakibn18 <jekben815@gmail.com>
2025-05-05 20:49:54 +0700

    deklarasi nama

--- friendClassDua.cpp
+++ friendClassDua.cpp
@@ -2,7 +2,24 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+  private:
+    string nama;
+  
+  public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+  a.nama = b;
+  cout << a.nama;
+}
+
 int main()
 {
-  
+  mahasiswa joko;
+  setNama(joko, "Kairi Kumar");
+  return 0;
 }
