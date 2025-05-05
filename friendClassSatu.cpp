2c1159a24898a878feaf10e002d8f0b5111fa5b4
zakibn18 <jekben815@gmail.com>
2025-05-05 19:27:21 +0700

    membuat funtion siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -3,9 +3,40 @@
 using namespace std;
 
 class siswa;
+class orang
+{
+  private:
+    string nama;
 
+  public:
+    void setNama(string pNama);
+    friend class siswa;
+};
 
+class siswa
+{
+  private:
+    int id;
 
+  public:
+   void setId(int pId);
+   void displayAll(orang &a);
+};
+
+void orang::setNama(string pNama)
+{
+  nama = pNama;
+}
+
+void siswa::setId(int pId)
+{
+  id = pId;
+}
+
+void siswa::displayAll(orang &a)
+{
+  cout << id << endl << a.nama;
+}
 
 int main()
 {
