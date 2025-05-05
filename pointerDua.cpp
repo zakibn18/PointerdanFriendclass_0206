f61deefa7fe310ceeda52d5d39ba588be2e3b39a
zakibn18 <jekben815@gmail.com>
2025-05-05 19:05:16 +0700

    membuat class mahasiswa

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa
+{
+  public :
+    int nim;
+    void showNim()
+    {
+      cout << "No Induk : " << nim << endl;
+    }
+};
+
 int main()
 {
-  
+
 }
