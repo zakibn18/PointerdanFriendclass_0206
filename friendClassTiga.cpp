8b4101b5d68bcc2686bc09ecd41b18c50f5c02ee
zakibn18 <jekben815@gmail.com>
2025-05-05 20:59:04 +0700

    friend class 3

---
+++ friendClassTiga.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class pelajar;
+class manusia
+{
+  public:
+    void showNilaiPelajar(pelajar &x);
+};
+
+class pelajar
+{
+  private:
+    int nilai;
+
+  public:
+    pelajar() {nilai = 100;}
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+  cout << x.nilai;
+}
+
+int main()
+{
+  manusia budi;
+  pelajar pbudi;
+  budi.showNilaiPelajar(pbudi);
+  return 0;
+}
