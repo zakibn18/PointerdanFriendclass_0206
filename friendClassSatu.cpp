98e6413afea87a5f90d8948a466a82685ce1e29d
zakibn18 <jekben815@gmail.com>
2025-05-05 19:33:13 +0700

    deklarasi nama dan ID

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -40,5 +40,10 @@ void siswa::displayAll(orang &a)
 
 int main()
 {
-
+ orang joko;
+ joko.setNama("Joko Susilo");
+ siswa joko_siswa;
+ joko_siswa.setId(1);
+ joko_siswa.displayAll(joko);
+ return 0;
 }
